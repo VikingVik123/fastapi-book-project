e3dff43445291753913e7bcc9a1a6a844be541c9
VikingVik123 <vodimmegwa@gmail.com>
2025-02-19 11:03:53 +0100

    updated router

--- api/router.py
+++ api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-
+from api.routes import integration_conf
 from api.routes import books
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
+api_router.include_router(integration_conf.router)
